12a90fd10ee537f7514094cc3864687588d30c3a
Agustin <agustinoliver1998@gmail.com>
2021-06-28 19:56:20 -0300

    testing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue App"/>
+    <HelloWorld msg="Welcome to Your sole App"/>
   </div>
 </template>
 
